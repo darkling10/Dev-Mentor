4cf3b6db173111f82ccb058dcdb69a9277d3e4d3
Garry <gmisal2002@gmail.com>
2022-09-25 17:03:27 +0530

    done

---
+++ backend/models/Courses.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const CoursesSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        preference: {
+            type: [String],
+            required: true,
+        },
+        userType: {
+            type: String,
+            enum: ["admin", "user"],
+            default: "admin",
+        },
+        // tokens: [
+        //   {
+        //     token: {
+        //       type: String,
+        //       required: true,
+        //     },
+        //   },
+        // ],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// UserSchema.pre("save", function (next) {
+//   var salt = bcrypt.genSaltSync(10);
+//   if (this.password && this.isModified("password")) {
+//     this.password = bcrypt.hashSync(this.password, salt);
+//   }
+//   next();
+// });
+
+// UserSchema.methods.getAuthToken = async function (data) {
+//   let params = {
+//     id: this.id,
+//     email: this.email,
+//     name: this.name,
+//     userType : this.userType
+//   };
+//   var tokenValue = jwt.sign(params, process.env.JWTSECRETKEY, {
+//     expiresIn: "300000s",
+//   });
+//   this.tokens = this.tokens.concat({ token: tokenValue });
+//   await this.save();
+//   return tokenValue;
+// };
+
+module.exports = Courses = mongoose.model("Courses", CoursesSchema);
