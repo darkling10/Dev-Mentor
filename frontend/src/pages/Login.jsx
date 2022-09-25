f4d673dbc228123e7e3874e38573862b70add74e
vinit <khollam.vinit@gmail.com>
2022-09-25 13:13:47 +0530

    login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,7 +4,21 @@ function Login() {
   return (
     <div className='w-full min-h-screen grid grid-cols-2'>
 
-      <div className="col w-full min-h-screen">
+      <div className="col text-white  bg-primary flex justify-between items-start flex-col p-10 w-full min-h-screen">
+    
+      <div className="flex justify-start items-center">
+      <i class='bx bxl-dev-to text-3xl'></i>
+      <p> Mentor </p>
+      </div>
+      
+
+      <div className="text">
+        <h1 className='text-3xl font-bold'> Learn About New Courses and Interview Experiences </h1>
+      </div>
+
+      <div className="comment">
+        
+      </div>
 
       </div>
       <div className="col w-full min-h-screen ">
