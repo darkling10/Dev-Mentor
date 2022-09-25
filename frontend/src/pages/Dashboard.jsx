0430446d0ece69b38b69ad811a40df1e65c6fcd3
Shantanu <sborkar9545@gmail.com>
2022-09-25 13:46:22 +0530

    Header Done

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import profileImage from "../assets/Images/profile_image.svg";
+import { Header } from "../components/Header";
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <>
+      <Header/>
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
